73e787af0e0c44612b3b22e029c99e1c0588ee36
Guilherme Bigois <guilhermebigois@outlook.com>
2026-06-13 05:08:45 -0300

    chore: remove unused files for template project

--- reactLocation/src/App/index.jsx
+++ reactLocation/src/App/index.jsx
@@ -1,15 +1,5 @@
-import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from '../AppRoutes';
-import NavBar from '../NavBar';
 import './styles.css';
 
-const App = () => {
-    return (
-        <BrowserRouter>
-            <NavBar />
-            <AppRoutes />
-        </BrowserRouter>
-    );
-};
+const App = () => <h1>App</h1>;
 
 export default App;
--- reactLocation/src/BasicComponents/index.jsx
+++ reactLocation/src/BasicComponents/index.jsx
@@ -1,9 +1,7 @@
 const Home = () => <h1>Home</h1>;
 
+const About = () => <h1>About</h1>;
+
 const Contact = () => <h1>Contact</h1>;
 
-const Me = () => <h1>Me</h1>;
-
-const You = () => <h1>You</h1>;
-
-export { Home, Contact, Me, You };
+export { Home, About, Contact };
