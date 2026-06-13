a72a184f9a92b1d0c7eaff46181016efa5289fdf
Guilherme Bigois <guilhermebigois@outlook.com>
2026-06-13 17:15:37 -0300

    refactor: simplify PokemonList lamba function return

--- reactFetch/src/PokemonList/PokemonList.jsx
+++ reactFetch/src/PokemonList/PokemonList.jsx
@@ -4,9 +4,7 @@ import "./PokemonList.css";
 const PokemonList = ({ pokemonList }) => {
     return (
         <ul>
-            {pokemonList.map((pokemon, index) => {
-                return <Pokemon key={index} pokemon={pokemon} />;
-            })}
+            {pokemonList.map((pokemon, index) => <Pokemon key={index} pokemon={pokemon} />)}
         </ul>
     );
 };
